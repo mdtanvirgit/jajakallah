c3530b9d832d4e49d3b1fb5e6d552d474eb952ad
mdtanvir <freelancermdtanvir@gmail.com>
2022-07-08 23:33:32 +0600

    i am add another code my.java

--- my.java
+++ my.java
@@ -3,16 +3,11 @@ public class Hello {
     public static void main (string [] arges ) {
     
         system .out.printin("vs code is awsem");
+        greeting ("md tanvir ");
     }
-    
+   public static void display(string name){
+    system.out.printin("welcome" + name);
+   }
+ 
     
     }
-    public class Hello {
-
-        public static void main (string [] arges ) {
-        
-            system .out.printin("vs code is awsem");
-        }
-        
-        
-        }
