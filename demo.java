12ac205e0d732369ab263910f7e5192823440a8c
mdtanvir <freelancermdtanvir@gmail.com>
2022-07-09 07:48:24 +0600

    i am add another code

--- demo.java
+++ demo.java
@@ -1,4 +1,8 @@
 public class demo {
     public static void main (string[] args);
     system.out.printin("vs code is nice");
+    public static void gretting (string name ){
+
+        system.out.printin("welcome"+ name);
+    }
 }
